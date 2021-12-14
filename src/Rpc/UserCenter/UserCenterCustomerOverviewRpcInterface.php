2ed00079ea39796fbc513be653dda6494cd9a289
wumahoo <471155250@qq.com>
2021-12-14 15:34:36 +0800

    feat:customer overview rpc

---
+++ src/Rpc/UserCenter/UserCenterCustomerOverviewRpcInterface.php
@@ -0,0 +1,86 @@
+<?php declare(strict_types=1);
+/**
+ * This file is part of Agarwood Cloud.
+ *
+ * @link     https://www.agarwood-cloud.com
+ * @document https://www.agarwood-cloud.com/docs
+ * @contact  676786620@qq.com
+ * @author   agarwood
+ */
+
+namespace Agarwood\Rpc\UserCenter;
+
+interface UserCenterCustomerOverviewRpcInterface
+{
+    /**
+     * Customer Service List
+     *
+     * @param int $officialAccountsId
+     * @param array  $filter
+     *
+     * @return array
+     */
+    public function customerList(int $officialAccountsId, array $filter): array;
+
+    /**
+     * Number of fans grabbed
+     *
+     * @param int $officialAccountsId
+     * @param array  $customerId
+     * @param string $startAt
+     * @param string $endAt
+     *
+     * @return array
+     */
+    public function obtainFans(int $officialAccountsId, array $customerId, string $startAt, string $endAt): array;
+
+    /**
+     * Total number of fans, including unsubscribe
+     *
+     * @param int $officialAccountsId
+     * @param array  $customerId
+     * @param string $startAt
+     * @param string $endAt
+     *
+     * @return array
+     */
+    public function fans(int $officialAccountsId, array $customerId, string $startAt, string $endAt): array;
+
+    /**
+     * unsubscribe
+     *
+     * @param int $officialAccountsId
+     * @param array  $customerId
+     * @param string $startAt
+     * @param string $endAt
+     *
+     * @return array
+     */
+    public function unsubscribe(int $officialAccountsId, array $customerId, string $startAt, string $endAt): array;
+
+    /**
+     * 在xx 时间段内 关注 且 有成交 的粉丝的 openid
+     *
+     * @param int $officialAccountsId
+     * @param array  $customerId
+     * @param array  $openid
+     * @param string $startAt
+     * @param string $endAt
+     *
+     * @return array
+     */
+    public function salesNewFansOpenid(int $officialAccountsId, array $customerId, array $openid, string $startAt, string $endAt): array;
+
+    /**
+     * The number of followers who subscribe and had transactions in a certain period of time
+     *
+     * @param int $officialAccountsId
+     * @param array  $customerId
+     * @param array  $openid
+     * @param string $startAt
+     * @param string $endAt
+     *
+     * @return array
+     */
+    public function newFansCash(int $officialAccountsId, array $customerId, array $openid, string $startAt, string $endAt): array;
+}
